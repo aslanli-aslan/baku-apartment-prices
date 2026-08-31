57803f8e37f9f9b36ec44a19ecd694d1c4da660f
Aslanli Aslan <noiocaaa@gmail.com>
2026-09-01 00:17:30 +0400

    feat: add near duplicate checking function

---
+++ src/apartmentprices/eda.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+def check_near_duplicates(df: pd.DataFrame, diff_cols: list) -> pd.DataFrame:
+
+    df = df.copy()
+
+    # must haves
+    KEYS = ["id", "updatedAt"]
+    diff_cols.extend(KEYS)
+
+    matching_cols = df.columns.difference(diff_cols)
+    base_cols = df.columns.difference(KEYS)
+
+    duplicates = df.duplicated(matching_cols, keep=False)
+    base_duplicates = df.duplicated(base_cols, keep=False)
+
+    # to investigate rows that have different "diff_cols"
+    diff_duplicates = duplicates & ~base_duplicates
+
+    return df[diff_duplicates].sort_values(list(matching_cols))
